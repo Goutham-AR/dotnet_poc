643a1f78c5ac3a6fa01340b4f4c37013ecd18f86
gouthamr <gouthamr@zero-pixels.com>
2025-02-25 14:44:12 +0530

    update hub

--- Hubs/TestHub.cs
+++ Hubs/TestHub.cs
@@ -12,24 +12,6 @@ public class TestHub : Hub
         _userService = service;
     }
 
-    public override Task OnConnectedAsync()
-    {
-        string? user = Context.GetHttpContext().Request.Headers["id"];
-        System.Console.WriteLine(user);
-        if (!string.IsNullOrEmpty(user)) 
-        {
-            _userService.AddUser(user, Context.ConnectionId);
-        }
-        return base.OnConnectedAsync();
-
-    }
-
-    public override Task OnDisconnectedAsync(Exception? exception)
-    {
-        _userService.RemoveConnectionId(Context.ConnectionId);
-        return base.OnDisconnectedAsync(exception);
-    }
-
     public async Task SendMessage(string user, string message)
     {
         await Clients.All.SendAsync("test", user, message);
